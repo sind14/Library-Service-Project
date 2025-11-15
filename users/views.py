a3e3c9f992b6d2eaf204b4825d21e150503b558f
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-11-15 16:02:01 +0100

    feat: add UserViewSet with permission method

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        if self.action in ["list", "destroy"]:
+            return [IsAdminUser()]
+
+        return super().get_permissions()
