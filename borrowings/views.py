e637e3c2035f2d56169897c0d864172993430c28
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-11-16 19:40:32 +0100

    feat: refactor BorrowingListView with get_serializer_class and get_queryset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,12 +1,23 @@
 from rest_framework import generics, permissions
 from .models import Borrowing
-from .serializers import  BorrowingReadSerializer
+from .serializers import  BorrowingReadSerializer, BorrowingCreateSerializer
 
 
-class BorrowingListView(generics.ListAPIView):
+class BorrowingListView(generics.ListCreateAPIView):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingReadSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        qs = Borrowing.objects.all()
+        if not user.is_staff:
+            qs = qs.filter(user=user)
+        return qs
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
