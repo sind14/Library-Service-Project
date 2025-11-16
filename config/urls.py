4abd88da6b9ba63746e9d688bc701bbb05a12a05
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-11-16 19:16:20 +0100

    feat: add borrowings app urls to project config

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls")),
-    path("api/", include("users.urls")),
+    path("api/books/", include("books.urls")),
+    path("api/users/", include("users.urls")),
+    path("api/borrowings", include("borrowings.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
 ]
