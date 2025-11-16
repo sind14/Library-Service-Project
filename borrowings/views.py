177ae94c5a81b3090c84b2001d585919fda06c4e
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-11-16 19:14:56 +0100

    feat: add list and detail views with permissions

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Borrowing
+from .serializers import  BorrowingReadSerializer
 
-# Create your views here.
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [permissions.IsAuthenticated]
